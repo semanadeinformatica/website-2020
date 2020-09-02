09df84795c5f8de86c893908b40f8a824e95f981
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-09-02 11:02:38 +0100

    Add "about us" photos

---
+++ src/components/hooks/about-us-query.js
@@ -0,0 +1,21 @@
+import { useStaticQuery, graphql } from "gatsby"
+
+export const useAboutUsImages = () => {
+  const data = useStaticQuery(graphql`
+    query AboutUsQuery {
+      allFile(filter: { relativeDirectory: { eq: "about-us" } }) {
+        edges {
+          node {
+            absolutePath
+            childImageSharp {
+              fixed(height: 250) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return data
+}
