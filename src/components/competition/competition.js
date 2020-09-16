0342309dd1defb07a9d5c7b23d04d84395fc47e9
Moisés Rocha <moisesrocha99@gmail.com>
2020-09-16 17:30:36 +0100

    updated layout of competition page

--- src/components/competition/competition.js
+++ src/components/competition/competition.js
@@ -10,7 +10,7 @@ import CompetitionStyles from "../../styles/competition/competition.module.css"
 
 const Competition = () => (
   <div>
-    <Container>
+    <Container className={CompetitionStyles.competitionContainer}>
       <Row>
         <Col
           xs="12"
@@ -32,72 +32,73 @@ const Competition = () => (
       </Row>
     </Container>
 
-    <div className={CompetitionStyles.conditions + " pt-4"}>
+    <div className={CompetitionStyles.orange_area}>
       <Container>
-        <Row>
-          <Col className={CompetitionStyles.conditionsText}>
-            Condições
-            <hr align="left" className={CompetitionStyles.horizontalRuler} />
-          </Col>
+        <Row className={CompetitionStyles.prizeSquares}>
+          <Prize
+            type="first"
+            title="Trotinete elétrica"
+            description="STOREX RIDE 80XL PRETA"
+          />
+          <Prize
+            type="second"
+            title="Headphones Bluetooth"
+            description="JBL Live 650BTNC"
+          />
+          <Prize type="third" title="Cartão oferta Fnac 100€" description="" />
         </Row>
-        <Row>
-          <Col xs="12" className={CompetitionStyles.conditionList}>
-            <ul>
-              <li>
-                A Competição terá início dia 27 de Outubro às 15h00, decorrendo
-                durante os 3 próximos dias até dia 30 de Outubro às 17:00.
-              </li>
-              <li>
-                A competição decorrerrá online, através da plataforma
-                HackerRank.
-              </li>
-              <li>
-                Serão lançados diariamente 2 novos desafios: dia 27, às 15h00 e
-                dias 28 e 29, às 21h00.
-              </li>
-              <li>
-                Apenas estarás habilitado aos prémios caso tenhas tenhas
-                realizado <strong>inscrição </strong>e <strong>check-in</strong>{" "}
-                na SINF.
-              </li>
-              <li>
-                A entrega de prémios será realizada durante a Sessão de
-                Encerramento.
-              </li>
-              <li>
-                A cópia de código é estritamente proibida, sendo esta
-                monitorizada pela própria plataforma HackerRank.
-              </li>
-              <li>
-                A pontuação em cada exercício será influenciada pela qualidade
-                da solução, sendo o factor de desempate o tempo demorado a
-                submeter a resolução.
-              </li>
-              <li>
-                Caso sejas um dos vencedores, certifica-te que estás presente na
-                Sessão de Encerramento ou indica-nos o contacto de alguém que
-                possa receber o prémio por ti.
-              </li>
-            </ul>
-          </Col>
-        </Row>
+        <div className="pt-4">
+          <Row>
+            <Col className={CompetitionStyles.conditionsText}>
+              Regulamento
+              <hr align="left" className={CompetitionStyles.horizontalRuler} />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="12" className={CompetitionStyles.conditionList}>
+              <ul>
+                <li>
+                  A Competição terá início dia 27 de Outubro às 15h00,
+                  decorrendo durante os 3 próximos dias até dia 30 de Outubro às
+                  17:00.
+                </li>
+                <li>
+                  A competição decorrerrá online, através da plataforma
+                  HackerRank.
+                </li>
+                <li>
+                  Serão lançados diariamente 2 novos desafios: dia 27, às 15h00
+                  e dias 28 e 29, às 21h00.
+                </li>
+                <li>
+                  Apenas estarás habilitado aos prémios caso tenhas tenhas
+                  realizado <strong>inscrição </strong>e{" "}
+                  <strong>check-in</strong> na SINF.
+                </li>
+                <li>
+                  A entrega de prémios será realizada durante a Sessão de
+                  Encerramento.
+                </li>
+                <li>
+                  A cópia de código é estritamente proibida, sendo esta
+                  monitorizada pela própria plataforma HackerRank.
+                </li>
+                <li>
+                  A pontuação em cada exercício será influenciada pela qualidade
+                  da solução, sendo o factor de desempate o tempo demorado a
+                  submeter a resolução.
+                </li>
+                <li>
+                  Caso sejas um dos vencedores, certifica-te que estás presente
+                  na Sessão de Encerramento ou indica-nos o contacto de alguém
+                  que possa receber o prémio por ti.
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
-    <Container className={CompetitionStyles.whiteSection}>
-      <Row className={CompetitionStyles.prizeSquares}>
-        <Prize
-          type="first"
-          title="Trotinete elétrica"
-          description="STOREX RIDE 80XL PRETA"
-        />
-        <Prize
-          type="second"
-          title="Headphones Bluetooth"
-          description="JBL Live 650BTNC"
-        />
-        <Prize type="third" title="Cartão oferta Fnac 100€" description="" />
-      </Row>
-    </Container>
 
     <Participate link="https://www.hackerrank.com/sinf-2019" />
   </div>
