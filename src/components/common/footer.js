3bbca97ee7f2659afd7352d29cc36c6c0036d502
Moisés Rocha <moisesrocha99@gmail.com>
2020-09-19 14:03:36 +0100

    made dark footer optional

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -2,8 +2,12 @@ import React from "react"
 
 import FooterStyles from "../../styles/common/footer.module.css"
 
-const Footer = () => (
-  <div className={FooterStyles.footerContainer + " py-2 py-sm-3 text-center"}>
+const Footer = ({ darkMode }) => (
+  <div
+    className={`${FooterStyles.footerContainer} ${
+      darkMode ? FooterStyles.dark : FooterStyles.light
+    } py-2 py-sm-3 text-center`}
+  >
     Copyright © 2020 niaefeup, todos os direitos reservados
   </div>
 )
--- src/pages/equipa.js
+++ src/pages/equipa.js
@@ -11,30 +11,12 @@ import PageBanner from "../components/utils/page_banner"
 
 import TeamStyles from "../styles/team/team.module.css"
 
-const determine_first = (teams, n_per_row) => {
-  const odd_row = [true]
-  let rows = 0
-  for (const team of teams) {
-    const element = team.members
-    rows += Math.ceil(element.length / n_per_row)
-
-    if (rows % 2 === 0) {
-      odd_row.push(true)
-    } else {
-      odd_row.push(false)
-    }
-  }
-
-  return odd_row
-}
-
 const TeamPage = ({ data }) => {
   const teams = data.allTeamJson.nodes
   const n_per_row = 4
-  const is_starting_row_odd = determine_first(teams, n_per_row)
 
   return (
-    <Layout>
+    <Layout darkFooter>
       <SEO title="Equipa" />
       <PageBanner>
         <h1 className={TeamStyles.team_page_title}>Equipa</h1>
