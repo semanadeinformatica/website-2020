091e5dfbb38dd887002602aa84d5a02be3be6d86
Moisés Rocha <moisesrocha99@gmail.com>
2020-10-06 18:37:08 +0100

    made footer dark

--- src/pages/competicao-programacao.js
+++ src/pages/competicao-programacao.js
@@ -7,7 +7,7 @@ import Competition from "../components/competition/competition"
 import PageBanner from "../components/utils/page_banner"
 
 const CompetitionPage = () => (
-  <Layout>
+  <Layout darkFooter>
     <SEO title="Competição de programação" />
     <PageBanner>
       <h1>Competição de Programação</h1>
