ca386ddf19f967899dc16da9bbf74aebfbeb67ae
Moisés Rocha <moisesrocha99@gmail.com>
2020-10-11 08:58:39 +0100

    Data - Team members (#12)

--- src/pages/equipa.js
+++ src/pages/equipa.js
@@ -47,6 +47,7 @@ export const pageQuery = graphql`
           linkedin
           name
           img {
+            base
             childImageSharp {
               fluid {
                 ...GatsbyImageSharpFluid
